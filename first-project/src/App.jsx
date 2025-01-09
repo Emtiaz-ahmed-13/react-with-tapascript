b07c6df330d2d502e14b7c1cbe9687ce372164b5
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-01-10 01:52:20 +0600

    done-day-2

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center">
         <h1 className="text-xl m-2 text-green-500 font-bold text-center bg-purple-500 p-4 rounded-lg shadow-lg">
-          We are here to learn React from TypeScript
+          We are here to learn React from tapaScript
         </h1>
       </div>
     </>

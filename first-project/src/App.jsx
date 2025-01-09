a4b804e49555057d78a325edc33ef93df6e9aa48
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-01-10 00:40:27 +0600

    done-day-2

---
+++ first-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 flex items-center justify-center">
+        <h1 className="text-xl m-2 text-green-500 font-bold text-center bg-purple-500 p-4 rounded-lg shadow-lg">
+          We are here to learn React from TypeScript
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
